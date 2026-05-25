250c4f75e5229da0f28228986c75dbfab5c986d0
Rajeswariankem <rajiankem0410@gmail.com>
2026-05-26 00:06:52 +0530

    updated

--- src/main/java/com/raaji/eventmanagement/EventmanagementApplication.java
+++ src/main/java/com/raaji/eventmanagement/EventmanagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventmanagementApplication {
 
+	//main method
 	public static void main(String[] args) {
 		SpringApplication.run(EventmanagementApplication.class, args);
 		System.out.println("Event Management Application Started");
